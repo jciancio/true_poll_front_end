82fcb86025152c479598492cad1e1de20a15b056
jciancio <jciancio7@gmail.com>
2016-12-03 19:21:08 -0500

    implemented voting buttons, updating vote count on ajax success

--- app/controllers/bill.js
+++ app/controllers/bill.js
@@ -2,12 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    voteFor(){
-      console.log('im here');
+    voteFor(bill){
+      this.vote(bill, 1);
     },
 
-    voteAgainst(){
-      console.log('im here');
+    voteAgainst(bill){
+      this.vote(bill, 0);
     }
+  },
+
+  vote(bill, position) {
+    $.ajax({
+      type: 'POST',
+      url: `/bills/${bill.id}/votes`,
+      contentType: 'application/json',
+      data: JSON.stringify({
+        vote: {
+          position: position,
+          bill_id: bill.id
+        }
+      })
+    })
+    .done(function(result){
+      let votesFor = result.bill.votes_for;
+      let votesAgainst = result.bill.votes_against;
+
+      $('#votesFor').html(votesFor);
+      $('#votesAgainst').html(votesAgainst);
+    });
   }
 });
